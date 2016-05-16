82bc489655a696b53583bc821ece2c539a2f8f85
brolly759 <brolly759@gmail.com>
2016-05-16 16:08:43 -0400

    Update SHT31test.ino

--- firmware/examples/SHT31test.ino
+++ firmware/examples/SHT31test.ino
@@ -4,26 +4,25 @@
 
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
 
-
 void setup() {
-#ifndef ESP8266
-  while (!Serial);     // will pause Zero, Leonardo, etc until serial console opens
-#endif
   Serial.begin(115200);
   Serial.println("SHT31 test");
   if (! sht31.begin(0x44)) {   // Set to 0x45 for alternate i2c addr
     Serial.println("Couldn't find SHT31");
-    while (1) delay(1);
+    }
   }
-}
-
 
 void loop() {
+    
   float t = sht31.readTemperature();
   float h = sht31.readHumidity();
+  float tF = (t* 9) /5 + 32;
 
   if (! isnan(t)) {  // check if 'is not a number'
+     //Temperature in C
     Serial.print("Temp *C = "); Serial.println(t);
+    //Temperature in F
+    Serial.print("Temp *F = "); Serial.println(tF);
   } else { 
     Serial.println("Failed to read temperature");
   }
